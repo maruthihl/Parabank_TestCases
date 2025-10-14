19f04c4e4f5bb85df9a42cc83048a770d0f35cf7
maruthihl <maruthihl2000@gmail.com>
2025-10-14 10:41:22 +0530

    Implemented framework using OOPS concepts

---
+++ src/main/java/com/example/parabank/base/BaseTest.java
@@ -0,0 +1,55 @@
+package com.example.parabank.base;
+
+import com.example.parabank.utils.ConfigReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    private static WebDriver driver;
+
+    public static void initializeBrowser() {
+
+        // Load browser from config
+        String browser = ConfigReader.getProperty("browser");
+        System.out.println("Browser from config: " + browser);  // Debug print
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else {
+            System.out.println("Browser not supported");
+            return;
+        }
+
+        driver.manage().window().maximize();
+
+        // Load URL from config
+        String url = ConfigReader.getProperty("url");
+        driver.get(url);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
